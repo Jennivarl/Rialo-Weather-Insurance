dc68ddbf005477a8f464ee9a40505f58618e135a
varl999 <aseluconfidence@gmail.com>
2026-03-09 04:07:43 +0100

    Simplify test endpoint

--- api/test.js
+++ api/test.js
@@ -1,3 +1,3 @@
-module.exports = function handler(req, res) {
-    res.status(200).json({ message: 'API is working!', timestamp: new Date().toISOString() });
+module.exports = (req, res) => {
+    res.json({ ok: true });
 };
